b0c0a6a38ef63818319748e8939be3f8a4ce7d9c
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-05-25 11:55:30 +0200

    Finishes advanced

---
+++ advanced/exercise1.js
@@ -0,0 +1,33 @@
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+var c = Number (c);
+
+var d = '36';
+var d = Number (d);
+
+var e = 42;
+
+
+var sum = a + b + c + d + e
+console.log(sum)
+
+var f = '1';
+var f = Number (f);
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+var i = Number (i);
+
+var mult = f * g * h * i
+console.log(mult);
+
+var division = mult / sum;
+
+console.log(division);
+document.write(division);
